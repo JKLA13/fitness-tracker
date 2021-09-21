16177f6d83bcbd6ec09eb094cbbf2a849907b680
Jacob Adamson <jakeladamson@gmail.com>
2021-09-20 19:15:26 -0600

    setup boilerplate for server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+//require modules
+const express = require("express");
+const mongojs = require("mongojs");
+const logger = require("morgan");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+// need to setup db on server
+
+// need routes for api and homepage
+
+// Listen on port 3000
+app.listen(3000, () => {
+  console.log("App running on port 3000!");
+});
