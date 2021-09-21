3b0cfdd935939af90592917a8b0d692b8f161203
Jacob Adamson <jakeladamson@gmail.com>
2021-09-20 20:18:04 -0600

    code for home html routes

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,21 @@
+// require mods, paths
+const path = "path";
+const express = require("express");
+const router = express.Router();
+
+//get home routes
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+//get stats html route
+router.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+//get exercis html route
+router.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+//export routes
+
+module.exports = router;
